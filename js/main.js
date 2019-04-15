fcf30bf61c1503c375cbe75829015e395865ef57
hsuallan <33412402+hsuallan@users.noreply.github.com>
2019-04-16 01:45:47 +0800

    finish all sidebar

---
+++ js/main.js
@@ -0,0 +1,35 @@
+var app = new Vue({
+      el: '#app',
+      data(){
+        return{
+          isclosed:false,
+          msg:'hello vue',
+          closedstyle:{
+            display:'none'
+          },
+          styletest:{
+            width:'230px'
+          },
+          easy:{
+            a:'文章標題',
+            b:'搜尋文章',
+            c:'地球24小時',
+            d:'地球商號',
+            e:'合作夥伴',
+            f:'會員登入',
+            g:'我的收藏',
+            h:'facebook 粉絲團',
+            i:' Google + 專頁',
+            j:'LINE@生活圈',
+            k:'關於地球圖輯隊',
+            l:'使用說明',
+          }
+        }
+      },
+      methods:{
+        simple:function () {
+          this.isclosed = !this.isclosed;
+          this.closedstyle.display = 'block';
+        }
+      }
+      })
